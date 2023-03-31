f1f2cceb38b44e5fc1e2df4b4bbfd5f03f8c6b37
ayyubnurumi <ayyubinvestment@gmail.com>
2023-03-31 18:40:20 +0700

    update home

--- src/pages/MainLayout.jsx
+++ src/pages/MainLayout.jsx
@@ -4,9 +4,9 @@ import { Link, Outlet } from "react-router-dom";
 export const MainLayout = () => {
   return (
     <>
-      <nav className="navbar navbar-expand-lg fixed-top bg-body-tertiary border-bottom border-dark">
+      <nav className="navbar navbar-expand-lg fixed-top bg-dark border-bottom border-dark">
         <div className="container-fluid">
-          <Link className="navbar-brand" href="#">
+          <Link className="navbar-brand text-light" to="">
             Medium
           </Link>
           <button
@@ -23,19 +23,19 @@ export const MainLayout = () => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" href="#">
+                <Link className="nav-link text-light active" aria-current="page" to="">
                   Home
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" href="#">
+                <Link className="nav-link text-light" to="">
                   Link
                 </Link>
               </li>
               <li className="nav-item dropdown">
                 <Link
                   className="nav-link dropdown-toggle"
-                  href="#"
+                  to=""
                   role="button"
                   data-bs-toggle="dropdown"
                   aria-expanded="false"
@@ -44,12 +44,12 @@ export const MainLayout = () => {
                 </Link>
                 <ul className="dropdown-menu">
                   <li>
-                    <Link className="dropdown-item" href="#">
+                    <Link className="dropdown-item" to="">
                       Action
                     </Link>
                   </li>
                   <li>
-                    <Link className="dropdown-item" href="#">
+                    <Link className="dropdown-item" to="">
                       Another action
                     </Link>
                   </li>
@@ -57,7 +57,7 @@ export const MainLayout = () => {
                     <hr className="dropdown-divider" />
                   </li>
                   <li>
-                    <Link className="dropdown-item" href="#">
+                    <Link className="dropdown-item" to="">
                       Something else here
                     </Link>
                   </li>
@@ -82,18 +82,18 @@ export const MainLayout = () => {
         </div>
       </nav>
       <Outlet />
-      <footer className="container-fluid bg-dark ps-4 pt-2">
-        <Link style={{color: 'whitesmoke', fontSize: '1.7rem'}} className="navbar-brand" href="#">
+      <footer className="container-fluid bg-dark pt-2">
+        <Link style={{fontSize: '1.7rem'}} className="navbar-brand text-light" to="">
           Medium
         </Link>
         <nav className="nav">
-          <Link className="nav-link link-light ps-0" href="#">
+          <Link className="nav-link link-light ps-0" to="">
             About
           </Link>
-          <Link className="nav-link link-light ps-0" href="#">
+          <Link className="nav-link link-light ps-0" to="">
             Help
           </Link>
-          <Link className="nav-link link-light ps-0" href="#">
+          <Link className="nav-link link-light ps-0" to="">
             Terms
           </Link>
           <Link className="nav-link link-light ps-0" to='' >Privacy</Link>
--- src/pages/main/Home.jsx
+++ src/pages/main/Home.jsx
@@ -1,22 +1,50 @@
 import React from "react";
+import pic from "../../assets/react.svg";
 
 export const Home = () => {
   return (
     <>
       <div
         style={{ height: "100vh" }}
-        className="container-fluid ps-4 bg-warning d-flex flex-column justify-content-center align-items-start border-bottom border-dark"
+        className="container-fluid bg-warning d-flex flex-column justify-content-center align-items-start border-bottom border-dark"
       >
-        <h1 style={{fontSize: '4.5rem', fontFamily: "serif"}}>Stay Curious.</h1>
-        <p style={{fontSize: '1.5rem', lineHeight: '1.5rem'}} className="mt-4 mb-5">
+        <h1 style={{ fontSize: "4.5rem", fontFamily: "serif" }}>
+          Stay Curious.
+        </h1>
+        <p
+          style={{ fontSize: "1.5rem", lineHeight: "1.5rem" }}
+          className="mt-4 mb-5"
+        >
           Discover stories, thinking, and expertise from writers on any topic.
         </p>
         <button className="btn btn-dark rounded-pill px-5">
           Start Reading
         </button>
       </div>
-      <div style={{height: '100vh'}}>
-        belum dibuat, tunggu yaa
+      <div style={{ height: "100vh" }} className="p-4">
+        <div className="card card-body" style={{ minHeight: "9rem" }}>
+          <div style={{ maxHeight: "2rem" }} className="d-flex">
+            <img
+              src={pic}
+              alt="avatar"
+              className="rounded-circle me-2"
+              style={{ maxHeight: "1.7rem" }}
+            />
+            <p>Author's Name - posting date</p>
+          </div>
+          <div className="row">
+            <h5 className="card-title text-truncate col">
+              Card title. Some quick example text to build on the card title and
+              make up the bulk of the card's content.
+            </h5>
+            <img
+              src={pic}
+              alt="pic"
+              className="col-3"
+              style={{ maxHeight: "7rem" }}
+            />
+          </div>
+        </div>{" "}
       </div>
     </>
   );
