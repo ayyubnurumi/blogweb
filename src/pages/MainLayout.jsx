dec8f2ab89e018366b316b5cae9e1efdfd94d834
ayyubnurumi <ayyubinvestment@gmail.com>
2023-04-03 15:50:37 +0700

    update ui

--- src/pages/MainLayout.jsx
+++ src/pages/MainLayout.jsx
@@ -4,12 +4,12 @@ import { Link, Outlet } from "react-router-dom";
 export const MainLayout = () => {
   return (
     <>
-      <nav className="navbar navbar-expand-lg fixed-top bg-warning border-bottom border-dark">
+      <nav className="navbar navbar-expand fixed-top bg-warning border-bottom border-dark">
         <div className="container-fluid">
           <Link className="navbar-brand" to="">
-            Medium
+            Medusa
           </Link>
-          <button
+          {/* <button
             className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
@@ -19,55 +19,35 @@ export const MainLayout = () => {
             aria-label="Toggle navigation"
           >
             <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          </button> */}
+          {/* <div className="collapse navbar-collapse" id="navbarSupportedContent"> */}
+            <ul className="nav navbar-nav ms-auto gap-2">
               <li className="nav-item">
-                <Link className="nav-link" to="">
-                  Home
+                <Link className="nav-link d-none d-md-block" to="">
+                  Our story
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="">
-                  Link
+                <Link className="nav-link d-none d-md-block" to="">
+                  Membership
                 </Link>
               </li>
-              <li className="nav-item dropdown">
+              <li className="nav-item">
                 <Link
-                  className="nav-link dropdown-toggle"
+                  className="nav-link d-none d-md-block"
                   to=""
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
                 >
-                  Dropdown
+                  Write
                 </Link>
-                <ul className="dropdown-menu">
-                  <li>
-                    <Link className="dropdown-item" to="">
-                      Action
-                    </Link>
-                  </li>
-                  <li>
-                    <Link className="dropdown-item" to="">
-                      Another action
-                    </Link>
-                  </li>
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                  <li>
-                    <Link className="dropdown-item" to="">
-                      Something else here
-                    </Link>
-                  </li>
-                </ul>
+              </li>
+              <li className="nav-item d-none d-sm-block">
+                <Link className="nav-link">Sign In</Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link disabled">Disabled</Link>
+                <Link className="nav-link btn btn-dark rounded-pill px-3 text-light">Get Started</Link>
               </li>
             </ul>
-            <form className="d-flex" role="search">
+            {/* <form className="d-flex" role="search">
               <input
                 className="form-control me-2"
                 type="search"
@@ -77,14 +57,14 @@ export const MainLayout = () => {
               <button className="btn btn-outline-success" type="submit">
                 Search
               </button>
-            </form>
-          </div>
+            </form> */}
+          {/* </div> */}
         </div>
       </nav>
       <Outlet />
-      <footer className="container-fluid bg-dark pt-2">
+      <footer className="container-fluid bg-dark pt-2 d-lg-none">
         <Link style={{fontSize: '1.7rem'}} className="navbar-brand text-light" to="">
-          Medium
+          Medusa
         </Link>
         <nav className="nav">
           <Link className="nav-link link-light ps-0" to="">
--- src/pages/main/Home.jsx
+++ src/pages/main/Home.jsx
@@ -9,12 +9,12 @@ export const Home = () => {
     { ava: pic, name: "david", postDate: "4 jan" },
     { ava: pic, name: "sanusi", postDate: "5 jan" },
     { ava: pic, name: "disa", postDate: "6 jan" },
-    { ava: pic, name: "aya", postDate: "7 jan" }
+    { ava: pic, name: "aya", postDate: "7 jan" },
   ];
   return (
     <>
       <div
-        style={{ height: "100vh" }}
+        style={{ height: "88vh" }}
         className="container-fluid bg-warning d-flex flex-column justify-content-center align-items-start border-bottom border-dark"
       >
         <h1 style={{ fontSize: "4.5rem", fontFamily: "serif" }}>
@@ -30,17 +30,20 @@ export const Home = () => {
           Start Reading
         </button>
       </div>
-      <div style={{ minHeight: "88vh" }} className="p-4">
-        {userDummy.map((item, i) => {
-          return (
-            <CardBlog
-              key={i}
-              user={item}
-              title="Ini adalah sebuah cerita romansa yang tidak biasa, jalannya tidak akan mudah, tentu butuh effort. this us against the world. semoga akhirnya baik-baik saja dan bahagia selama-lamanya"
-              img={pic}
-            />
-          );
-        })}
+      <div style={{ minHeight: "88vh" }} className="container row p-4">
+        <div className="col">
+          {userDummy.map((item, i) => {
+            return (
+              <CardBlog
+                key={i}
+                user={item}
+                title="Ini adalah sebuah cerita romansa yang tidak biasa, jalannya tidak akan mudah, tentu butuh effort. this us against the world. semoga akhirnya baik-baik saja dan bahagia selama-lamanya"
+                img={pic}
+              />
+            );
+          })}
+        </div>
+        <aside className="col-3 d-none d-md-block text-dark"><hr/></aside>
       </div>
     </>
   );
