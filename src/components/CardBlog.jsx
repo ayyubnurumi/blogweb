cc4e12a065d44794e21bced4e77ae0fe89f66f37
ayyubnurumi <ayyubinvestment@gmail.com>
2023-04-11 17:38:29 +0700

    update ui

--- src/components/CardBlog.jsx
+++ src/components/CardBlog.jsx
@@ -1,26 +1,50 @@
 import React from "react";
 import { Avatar } from "./Avatar";
 
-export const CardBlog = ({user, blog, id}) => {
+export const CardBlog = ({ user, blog, id }) => {
   return (
     <>
       <div
-        className="card card-body px-0 mb-1 border-0 rounded-0 border-bottom border-black"
+        className="card card-body px-0 mb-1 border-0"
         style={{ minHeight: "9rem" }}
         key={id}
       >
-        <div style={{ maxHeight: "2rem" }} className="d-flex align-content-center mb-1">
-          <Avatar user={user.ava} height={"1.5rem"} />
-          <p>{user.name} - {blog.postDate}</p>
-        </div>
         <div className="row">
-          <h5 className="card-title multiline-text-truncate col align-self-center">
-            {blog.title}
-          </h5>
+          <div className="col">
+            <div
+              style={{ maxHeight: "2rem" }}
+              className="d-flex align-content-center mb-1"
+            >
+              <Avatar user={user.ava} height={"1.5rem"} />
+              <p>
+                {user.name} - {blog.postDate}
+              </p>
+            </div>
+            <h5 className="card-title multiline-text-truncate-2 align-self-center">
+              {blog.title}
+            </h5>
+            <div className="d-flex mt-2">
+              <p>
+                {blog.postDate} - {blog.timeRead}
+              </p>
+              <svg
+                width="25"
+                height="25"
+                viewBox="0 0 25 25"
+                fill="none"
+                className="ms-auto"
+              >
+                <path
+                  d="M18 2.5a.5.5 0 0 1 1 0V5h2.5a.5.5 0 0 1 0 1H19v2.5a.5.5 0 1 1-1 0V6h-2.5a.5.5 0 0 1 0-1H18V2.5zM7 7a1 1 0 0 1 1-1h3.5a.5.5 0 0 0 0-1H8a2 2 0 0 0-2 2v14a.5.5 0 0 0 .8.4l5.7-4.4 5.7 4.4a.5.5 0 0 0 .8-.4v-8.5a.5.5 0 0 0-1 0v7.48l-5.2-4a.5.5 0 0 0-.6 0l-5.2 4V7z"
+                  fill="#292929"
+                ></path>
+              </svg>
+            </div>
+          </div>
           <img
             src={blog.img}
             alt="pic"
-            className="col-3"
+            className="col-3 col-lg-4 my-auto"
             style={{ maxHeight: "7rem" }}
           />
         </div>
--- src/pages/main/Home.jsx
+++ src/pages/main/Home.jsx
@@ -58,15 +58,44 @@ export const Home = () => {
 
       {/* -- trending section -- */}
       <div className="m-5">
-        <div>
-          <h2>Trending on Medium</h2>
+        <div className="d-flex">
+          <svg
+            width="28"
+            height="29"
+            viewBox="0 0 28 29"
+            fill="none"
+            className="mx-3"
+          >
+            <path fill="#fff" d="M0 .8h28v28H0z"></path>
+            <g opacity="0.8" clipPath="url(#trending_svg__clip0)">
+              <path fill="#fff" d="M4 4.8h20v20H4z"></path>
+              <circle cx="14" cy="14.79" r="9.5" stroke="#000"></circle>
+              <path
+                d="M5.46 18.36l4.47-4.48M9.97 13.87l3.67 3.66M13.67 17.53l5.1-5.09M16.62 11.6h3M19.62 11.6v3"
+                stroke="#000"
+                strokeLinecap="round"
+              ></path>
+            </g>
+            <defs>
+              <clipPath id="trending_svg__clip0">
+                <path
+                  fill="#fff"
+                  transform="translate(4 4.8)"
+                  d="M0 0h20v20H0z"
+                ></path>
+              </clipPath>
+            </defs>
+          </svg>
+          <h2 className="fs-4">Trending on Medium</h2>
         </div>
         <ol className="container-fluid row row-cols-1 row-cols-md-2 row-cols-lg-3">
           {userDummy.map((item, i) => {
             const blogItem = blogDummy[i];
             return (
               <li key={i} className="col d-flex">
-                <span className="mt-2 text-secondary h2 opacity-25">0{i+1}</span>
+                <span className="mt-2 text-secondary h2 opacity-25">
+                  0{i + 1}
+                </span>
                 <CardTrending user={item} blog={blogItem} />
               </li>
             );
@@ -85,27 +114,33 @@ export const Home = () => {
         </div>
         <div className="col-md-4 d-none d-md-block text-dark p-3">
           <div className="sticky-md-top" style={{ top: "4rem" }}>
-            <h2>Discover more of what matters to you</h2>
-            <div>
+            <h2 className="fs-5">Discover more of what matters to you</h2>
+            <div className="my-3">
               {topicDummy.map((item, i) => {
                 return (
                   <button
                     key={i}
-                    className="btn btn-secondary rounded-pill m-1"
+                    className="btn bg-secondary bg-opacity-25 text-capitalize rounded-pill m-1"
                   >
                     {item}
                   </button>
                 );
               })}
             </div>
-            <Link>See more topics</Link>
+            <Link className="text-success text-decoration-none" to={""}>
+              See more topics
+            </Link>
             <hr />
-            <div>
+            <div className="d-flex flex-wrap">
               {topicDummy.map((item, i) => {
                 return (
-                  <button key={i} className="btn m-1">
+                  <Link
+                    key={i}
+                    to={{}}
+                    className="m-1 lh-1 text-secondary text-decoration-none"
+                  >
                     {item}
-                  </button>
+                  </Link>
                 );
               })}
             </div>
